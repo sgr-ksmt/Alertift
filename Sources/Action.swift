1b60d81f427ac7fa69a428eb82a474fcce3a8414
Lennart Fischer <github@lambdadigamma.com>
2020-10-15 20:10:27 +0200

    Added Basic SPM Support

--- Sources/Action.swift
+++ Sources/Action.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension Alertift {
     /// Action type for **Alert**, **ActionSheet**
--- Sources/AlertType.swift
+++ Sources/AlertType.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 /// Internal AlertType protocol
 internal protocol _AlertType: class {
